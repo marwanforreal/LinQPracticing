94730a794646b1af14f7526f00cd628c488e3010
Marwan Al Khasawneh <marwankhasawneh@gmail.com>
2021-07-04 10:48:36 +0300

    Created a Select All Function

---
+++ LinqPracticing/LinqPracticing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqPracticing
+{
+    public static class LinqPracticing
+    {
+        public static void SelectAll(List<Product> Products)
+        {
+            Console.WriteLine("Select All");
+
+            var LinqList = Products
+                .Select(p => p)
+                .ToList();
+
+            Product.print(LinqList);
+        }
+    }
+}
